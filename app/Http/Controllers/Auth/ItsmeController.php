a033317dcf680efb89c022bd19bfe0f75ae19c83
Simon Depelchin <dep.simon@gmail.com>
2018-09-18 13:34:31 +0200

    Handle the callback, login & register + migration

--- app/Http/Controllers/Auth/ItsmeController.php
+++ app/Http/Controllers/Auth/ItsmeController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Zttp\Zttp;
 
 class ItsmeController extends Controller
@@ -21,5 +24,41 @@ class ItsmeController extends Controller
         session()->flash('transactionToken', $transaction['transactionToken']);
 
         return redirect($transaction['authenticationUrl']);
+    }
+
+    public function callback()
+    {
+        $response = Zttp::get(config('services.itsme.base_url') . 'status/' . session('transactionToken'))->json();
+
+        $data = [
+            'name' => data_get($response, 'name.fullName'),
+            'email' => data_get($response, 'emailAddress'),
+            'gender' => data_get($response, 'gender'),
+            'birthdate' => data_get($response, 'birthdate'),
+            'locale' => data_get($response, 'locale'),
+            'phone' => data_get($response, 'phoneNumber'),
+            'street' => data_get($response, 'address.streetAddress'),
+            'postal_code' => data_get($response, 'address.postalCode'),
+            'city' => data_get($response, 'address.city'),
+            'country_code' => data_get($response, 'address.countryCode'),
+        ];
+
+        if (request('service') == 'login') {
+            $user = User::where('itsme_id', data_get($response, 'userId'))->first();
+
+            $user->update($data);
+
+            Auth::login($user, true);
+
+            return redirect()->route('home');
+        } else {
+            $user = User::create(['itsme_id' => data_get($response, 'userId')] + $data);
+
+            event(new Registered($user));
+
+            Auth::login($user, true);
+
+            return redirect()->route('home');
+        }
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,23 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+
             $table->string('name');
             $table->string('email')->unique();
+
+            $table->string('gender')->nullable();
+            $table->date('birthdate')->nullable();
+            $table->string('locale', 2)->nullable();
+            $table->string('phone')->nullable();
+            $table->string('street')->nullable();
+            $table->string('postal_code')->nullable();
+            $table->string('city')->nullable();
+            $table->string('country_code')->nullable();
+
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->string('itsme_id')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
