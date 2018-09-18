f5abc49f058127ca11c3c0039af08023d93a8dc1
Simon Depelchin <dep.simon@gmail.com>
2018-09-18 13:31:58 +0200

    Handle the redirect

--- app/Http/Controllers/Auth/ItsmeController.php
+++ app/Http/Controllers/Auth/ItsmeController.php
@@ -2,10 +2,24 @@
 
 namespace App\Http\Controllers\Auth;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Zttp\Zttp;
 
 class ItsmeController extends Controller
 {
-    //
+    public function redirect()
+    {
+        $transaction = Zttp::post(config('services.itsme.base_url') . 'transactions', [
+            'token' => config('services.itsme.token'),
+            'service' => request('service', 'register'),
+            'scopes' => ['profile', 'email', 'phone', 'address'],
+            'locale' => 'fr',
+            'redirectUrl' => route('itsme.callback', ['service' => request('service', 'register')]),
+        ])->json();
+
+        session()->flash('transactionToken', $transaction['transactionToken']);
+
+        return redirect($transaction['authenticationUrl']);
+    }
 }
