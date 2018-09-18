29de923b092262bffdf4fe9a3b91a48043ad1346
Simon Depelchin <dep.simon@gmail.com>
2018-09-18 13:35:15 +0200

    Fixing register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -69,6 +69,7 @@
                             </div>
                         </div>
                     </form>
+                    <hr>
                     <div class="form-group row mb-0">
                         <div class="col-md-6 offset-md-4">
                             <a href="{{ route('itsme.redirect', ['service' => 'register']) }}" class="btn btn-primary">
